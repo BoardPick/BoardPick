34d5f32ec375000d48e33b16bffd1eff0bebb879
yoojin <yoojin4719@gmail.com>
2024-05-05 21:58:05 +0900

    FE Setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Router } from "react-router-dom";
 
 import "./App.scss";
 import OnBoarding from "./pages/OnBoarding/OnBoarding";
@@ -6,29 +6,34 @@ import Home from "./pages/Home/Home";
 import Category from "./pages/Category/Category";
 import Review from "./pages/Review/Review";
 
-import CategoryDetail from "./components/CategoryDetail/CategoryDetail";
+import CategoryDetail from "./pages/CategoryDetail/CategoryDetail";
 import MyPick from "./pages/MyPick/MyPick";
 import MyPage from "./pages/MyPage/MyPage";
-import TabBar from "./layouts/TabBar/TabBar";
+import NavigationBar from "./layouts/NavigationBar/NavigationBar";
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   return (
     <div className="App">
       <Routes>
         <Route
           path="/onBoarding"
-          element={isLoggedIn ? <Navigate to="/" /> : <OnBoarding />}
+          // element={isLoggedIn ? <Navigate to="/" /> : <OnBoarding />}
+          element={<OnBoarding />}
         />
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          // element={isLoggedIn ? <Home /> : <Navigate to="/onBoarding" />}
+          element={<Home />}
+        />
         <Route path="/category" element={<Category />} />
         <Route path="/review" element={<Review />} />
         <Route path="/category/:id" element={<CategoryDetail />} />
         <Route path="/myPick" element={<MyPick />} />
         <Route path="/myPage" element={<MyPage />} />
       </Routes>
-      {isLoggedIn && <TabBar />}
+      {isLoggedIn && <NavigationBar />}
     </div>
   );
 }
--- src/pages/OnBoarding/OnBoarding.js
+++ src/pages/OnBoarding/OnBoarding.js
@@ -1,12 +1,16 @@
 import React from "react";
 
+import kakaoLogin from "../../assets/image/kakao_login.png";
+
 const OnBoarding = () => {
   return (
     <div>
       로그인페이지
       <ul>
         <ul>
-          <li>카카오 계정으로 시작</li>
+          <li>
+            <img src={kakaoLogin} alt="kakao login" />
+          </li>
           <li>네이버 계정으로 시작</li>
           <li>구글 계정으로 시작</li>
         </ul>
