914c6d50b250cb71113d2d9d738655ee46efa7df
yoojin <yoojin4719@gmail.com>
2024-05-08 18:55:23 +0900

    FEAT : ToastPopUp component 완료

--- src/components/ToastPopUp/ToastPopUp.js
+++ src/components/ToastPopUp/ToastPopUp.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ToastPopUp = ({ ToastContent, viewBtn }) => {
+  const navigate = useNavigate();
   return (
     <div className="toastPopUp">
       <span>{ToastContent}</span>
-      {viewBtn && <button className="view">보기</button>}
+      {viewBtn && (
+        <button className="view" onClick={() => navigate("/myPick")}>
+          보기
+        </button>
+      )}
     </div>
   );
 };
