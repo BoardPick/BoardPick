c3fd3f70525c65071f294c0aa71f766bddf40aaa
yoojin <yoojin4719@gmail.com>
2024-05-11 01:57:33 +0900

    FEAT : AppBar component 완료

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -3,27 +3,27 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { ChevronLeft, Link, Bookmark } from "../../assets/icon/icon";
 
-const AppBar = ({ title, mark }) => {
+const AppBar = ({ title, mark, type }) => {
+  const BarType = ["gradient"].includes(type) ? type : "";
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const pick = useSelector((state) => state.pick);
   const setPick = () => {
     dispatch({ type: "SET_PICK", payload: !pick });
   };
-  const click = () => {
+  const handlePick = () => {
     setPick(!pick);
   };
-  console.log(pick);
 
   return (
-    <div className="AppBar">
+    <div className={`AppBar ${BarType}`}>
       <span className="back" onClick={() => navigate(-1)}>
         <ChevronLeft />
       </span>
-      <span>{title}</span>
+      <span className="barTitle">{title}</span>
       {mark && (
         <span className="leftBtns">
-          <button onClick={click}>
+          <button onClick={handlePick}>
             <Bookmark />
           </button>
           <button>
