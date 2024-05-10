ee4516d18e1c18177b26a49740784c2db17ea3fd
yoojin <yoojin4719@gmail.com>
2024-05-11 01:28:38 +0900

    FEAT : StarScore component 완료

--- src/components/StarScore/StarScore.js
+++ src/components/StarScore/StarScore.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { Star } from "../../assets/icon/icon.js";
 
-const StarScore = () => {
+const StarScore = ({ ver }) => {
+  const verType = ["black", "round"].includes(ver) ? ver : "";
+  const [rate, setRate] = useState("3");
   const reviewCount = useSelector((state) => state.reviewCount);
 
   return (
     <div className="avgStar">
       <span className="star">
-        {[...Array(parseInt(5))].map(() => (
-          <Star />
+        {Array.from({ length: 5 }, (_, i) => (
+          <Star key={i} />
         ))}
       </span>
-      <span className="avg">0.0</span>
-      <span className="reviewCount">({reviewCount})</span>
+      <span className={`avg ${verType}`}>{rate}.0</span>
+      <span className={`reviewCount ${verType}`}>({reviewCount})</span>
     </div>
   );
 };
