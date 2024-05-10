7207e97d44e99f46d1ca03519db3b2cb3be43561
yoojin <yoojin4719@gmail.com>
2024-05-10 17:54:42 +0900

    FEAT : NoticeBtn component  완료

--- src/components/Btn/NoticeBtn/NoticeBtn.js
+++ src/components/Btn/NoticeBtn/NoticeBtn.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { ChevronRight } from "../../../assets/icon/icon";
 
-const NoticeBtn = ({ text, notice }) => {
+const NoticeBtn = ({ text, notice, onClick }) => {
   const noticeType = ["notice"].includes(notice) ? notice : "";
   return (
-    <button className={`noticeBtn ${noticeType}`}>
+    <button className={`noticeBtn ${noticeType}`} onClick={onClick}>
       <article className={["noticeTxt", `noticeTxt_${noticeType}`].join(" ")}>
         <span className="noticeBadge">공지</span>
         <p>{text}</p>
