d7086804485231d6c5f14e2d7b8ccd9a9e0644b5
seonmin <seonmiki@student.42seoul.kr>
2024-05-14 01:14:28 +0900

    searchBar & categoryTab

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 // import { Search } from "../../assets/icon/icon.js";
 
 const SearchBar = () => {
+	const [text, setText] = useState("");
+
+	const onChange = (event) => {
+		setText(event.target.value);
+	}
+
+	const onSearch = () => {
+		// setSearch(text)
+	}
+
+	const handleKeyDown = (event) => {
+		if (event.key === "Enter") {
+				// setSearch(text);
+			}
+		}
 
 	return (
 	<div className="SearchBar">
 		<div className="searchBox">
-			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input"/>
-			<img className="img" src="src/assets/icon/search.svg" alt="" />
+			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input" value={text} onChange={onChange} onKeyDown={handleKeyDown}/>
+			<img className="img" src="src/assets/icon/search.svg" alt="" onClick={onSearch}/>
 			{/* <Search className="img"/> */}
 		</div>
 	</div>
