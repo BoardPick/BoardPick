b253cf2914ad5dfebe339e092af92ad9da0c51b4
seonmin <seonmiki@student.42seoul.kr>
2024-05-14 01:19:25 +0900

    Refact: searchBar 덜완성된 부분 주석처리

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -23,7 +23,7 @@ const SearchBar = () => {
 	<div className="SearchBar">
 		<div className="searchBox">
 			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input" />
-			<img className="img" src="src/assets/icon/search.svg" alt="" onClick={onSearch}/>
+			<img className="img" src="src/assets/icon/search.svg" alt="" />
 			{/* <Search className="img"/> */}
 		</div>
 	</div>
