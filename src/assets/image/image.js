c99263e67fe69a61c87d4a73f88ba6a1c0df764d
yoojin <yoojin4719@gmail.com>
2024-05-14 00:35:14 +0900

    FEAT : Review

---
+++ src/assets/image/image.js
@@ -0,0 +1,10 @@
+//profile
+export const profile_p = require("../image/profile/pink.png");
+export const profile_y = require("../image/profile/yellow.png");
+export const profile_g = require("../image/profile/green.png");
+export const profile_b = require("../image/profile/blue.png");
+export const profile_brand = require("../image/profile/brand.png");
+
+//charactor
+export const pix = require("../image/character/pix.png");
+export const pix_search = require("../image/character/pix_search.png");
