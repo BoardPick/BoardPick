7effbee28e38aea806860d01f198f9a0946be602
seonmin <seonmiki@student.42seoul.kr>
2024-05-14 00:18:05 +0900

    Refact: searchbar & category

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import SearchBar from "../../components/SearchBar/SearchBar.js"
-import Header from "./Header/Header.js"
+// import Header from "./Header/Header.js"
 // import Categoryjson from "./category.json"
 import {
   Strategy,
@@ -19,12 +19,14 @@ import {
 const Category = () => {
   const navigate = useNavigate();
   const [selectedCategory, setSelectedCategory] = useState("");
-  const categorys = [
+  const category1 = [
     { url: "/Category/strategy", icon: <Strategy />, text: "전략게임" },
     { url: "/Category/rollplaying", icon: <Rollplaying />, text: "롤플레잉" },
     { url: "/Category/cardgame", icon: <Cardgame />, text: "카드게임" },
     { url: "/Category/cooperation", icon: <Cooperation />, text: "협력게임" },
     { url: "/Category/deduction", icon: <Deduction />, text: "추리게임" },
+  ];
+  const category2 = [
     { url: "/Category/batting", icon: <Batting />, text: "베팅게임" },
     { url: "/Category/mafia", icon: <Mafia />, text: "마피아" },
     { url: "/Category/memory", icon: <Memory />, text: "기억력" },
@@ -39,19 +41,32 @@ const Category = () => {
 
   return (
     <div className="Categorys">
-      <Header />
+      {/* <Header /> */}
       <SearchBar />
-      <ul>
-        {categorys.map((tab, i) => (
-          <li
-            key={i}
-            className="category" onClick={() => handleCategoryClick(tab.url)}
-          >
-            <span className="categoryIcon">{tab.icon}</span>
-            <span className="categoryName">{tab.text}</span>
-          </li>
-        ))}
-      </ul>
+      <div className="categorybox">
+        <ul>
+          {category1.map((tab) => (
+            <li
+              key={tab.text}
+              className="category" onClick={() => handleCategoryClick(tab.url)}
+            >
+              <span className="categoryIcon">{tab.icon}</span>
+              <span className="categoryName">{tab.text}</span>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {category2.map((tab) => (
+            <li
+              key={tab.text}
+              className="category" onClick={() => handleCategoryClick(tab.url)}
+            >
+              <span className="categoryIcon">{tab.icon}</span>
+              <span className="categoryName">{tab.text}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/pages/Category/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div className="Header">카테고리</div>
-  )
-}
-
-export default Header
