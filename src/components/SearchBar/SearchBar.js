a7cdab13616805ef080bd79c58a0440353e1fc0b
seonmin <seonmiki@student.42seoul.kr>
2024-05-14 01:17:58 +0900

    Refact: conflict solve

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
 // import { Search } from "../../assets/icon/icon.js";
 
 const SearchBar = () => {
-	const [text, setText] = useState("");
+	// const [text, setText] = useState("");
 
-	const onChange = (event) => {
-		setText(event.target.value);
-	}
+// 	const onChange = (event) => {
+// 		setText(event.target.value);
+// 	}
 
-	const onSearch = () => {
-		// setSearch(text)
-	}
+// 	const onSearch = () => {
+// 		// setSearch(text)
+// 	}
 
-	const handleKeyDown = (event) => {
-		if (event.key === "Enter") {
-				// setSearch(text);
-			}
-		}
+// 	const handleKeyDown = (event) => {
+// 		if (event.key === "Enter") {
+// 				// setSearch(text);
+// 			}
+// 		}
 
 	return (
 	<div className="SearchBar">
 		<div className="searchBox">
-			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input" value={text} onChange={onChange} onKeyDown={handleKeyDown}/>
+			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input" />
 			<img className="img" src="src/assets/icon/search.svg" alt="" onClick={onSearch}/>
 			{/* <Search className="img"/> */}
 		</div>
