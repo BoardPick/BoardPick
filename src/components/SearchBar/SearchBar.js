a951e1a050c960efb3775d79075f5ec71fd6062e
seonmiki <seonmiki@student.42seoul.kr>
2024-05-14 14:18:21 +0900

    merge retry

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,28 +1,12 @@
 import React from 'react'
-// import { useState } from 'react'
 // import { Search } from "../../assets/icon/icon.js";
 
 const SearchBar = () => {
-	// const [text, setText] = useState("");
-
-// 	const onChange = (event) => {
-// 		setText(event.target.value);
-// 	}
-
-// 	const onSearch = () => {
-// 		// setSearch(text)
-// 	}
-
-// 	const handleKeyDown = (event) => {
-// 		if (event.key === "Enter") {
-// 				// setSearch(text);
-// 			}
-// 		}
 
 	return (
 	<div className="SearchBar">
 		<div className="searchBox">
-			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input" />
+			<input type="text" placeholder='PICK 하고싶은 보드게임을 검색해 주세요!' className="input"/>
 			<img className="img" src="src/assets/icon/search.svg" alt="" />
 			{/* <Search className="img"/> */}
 		</div>
