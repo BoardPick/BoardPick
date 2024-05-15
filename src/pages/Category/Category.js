01c9c3b00f7a8367be681c3dca206a30956932c7
seonmiki <seonmiki@student.42seoul.kr>
2024-05-15 21:28:40 +0900

    UI: 카테고리-인기검색태그

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "../../components/Search/SearchBar/SearchBar.js"
 import CategoryBox from "../../components/CategoryBox/CategoryBox.js"
+import Button from "../../components/Btn/Button/Button.js";
 
 const Category = () => {
 
@@ -8,6 +9,26 @@ const Category = () => {
     <div className="Categorys">
       <SearchBar value="\n"/>
       <CategoryBox />
+      <div className="HotTag">
+        <h1 className="title">가장 핫한 보드게임</h1>
+        <div className="tags">
+          <div className="tag">
+            <Button text={"#보드게임"} size={"s36"} type={"default"}></Button>
+          </div>
+          <div className="tag">
+            <Button text={"#보드게임"} size={"s36"} type={"default"}></Button>
+          </div>
+          <div className="tag">
+            <Button text={"#보드게임"} size={"s36"} type={"default"}></Button>
+          </div>
+          <div className="tag">
+            <Button text={"#보드게임"} size={"s36"} type={"default"}></Button>
+          </div>
+          <div className="tag">
+            <Button text={"#보드게임"} size={"s36"} type={"default"}></Button>
+          </div>
+        </div>
+      </div>
      </div>
   );
 };
