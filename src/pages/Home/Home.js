afb931ae2799ae757274624b697f33ac99f2c67c
seonmin <seonmiki@student.42seoul.kr>
2024-05-17 02:37:42 +0900

    검색바 클릭시 검색페이지로 이동 & 홈 보드픽랭킹 미니헤더

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
 import SearchBar from "../../components/Search/SearchBar/SearchBar";
-import SearchResult from "../../components/Search/SearchResult/SearchResult";
 import CategoryBox from "../../components/CategoryBox/CategoryBox";
 import Banner from "../../assets/image/banner.svg";
 
 const Home = () => {
-  const [onsearch, setOnsearch] = useState(false);
 
   return <div className="home">
     <SearchBar />
-    {onsearch === true ? <SearchResult keyworld={"검색 키워드"}/> :
-      <div className="wrapper">
-        <div className="banner">
-          <img src={Banner} alt="샘플이미지" onClick={() => setOnsearch(!onsearch)}/>
-        </div>
-        <CategoryBox />
+    <div className="banner">
+      <img src={Banner} alt="샘플이미지" />
+    </div>
+    <CategoryBox />
+
+    {/* 오늘의 보드픽 랭킹 */}
+    <div className="pickRank">
+      <div className="miniHeader">
+        <h1 className="title">오늘의 보드P!CK 랭킹</h1>
+        <p className="info">오늘 가장 많은 P!CK을 받은 게임들만 모아봤어요!</p>
       </div>
-    }
+    </div>
+        
+
   </div>;
 };
 
