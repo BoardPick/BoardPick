0b378c54c0b8c0dbd976436fee5ab223ef799b63
seonmiki <seonmiki@student.42seoul.kr>
2024-05-17 18:07:28 +0900

    보드픽 랭킹 슬라이드 커스텀 적용

--- src/components/ThumbNail/Rank/Rank.js
+++ src/components/ThumbNail/Rank/Rank.js
@@ -6,27 +6,13 @@ const Rank = () => {
   return (
     <div className="Rank">
 		<Swiper>
-		{[...Array(5)].map((_, i) => (
-			<SwiperSlide key={i}>
+		{[...Array(2)].map((_, i) => (
+			<SwiperSlide key={i} className="swiper-slide-custom">
 				<article className="thumImg">
 					<img src="http://placehold.it/39X46" alt="" />
 				</article>
 				<article className="thumName">
 					<h1 className="num">{i + 1}</h1>
-					<article className="text">
-						<h1>보드게임 타이틀</h1>
-						<p>보드게임 설명 텍스트 영역입니다.</p>
-					</article>
-				</article>
-			</SwiperSlide>
-		))}
-		{[...Array(5)].map((_, i) => (
-			<SwiperSlide key={i}>
-				<article className="thumImg">
-					<img src="http://placehold.it/39X46" alt="" />
-				</article>
-				<article className="thumName">
-					<h1 className="num">{i + 6}</h1>
 					<article className="text">
 						<h1>보드게임 타이틀</h1>
 						<p>보드게임 설명 텍스트 영역입니다.</p>
