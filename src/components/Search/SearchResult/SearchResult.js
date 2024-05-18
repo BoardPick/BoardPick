a86bef91584e500cf8c7891be6ef04cef41115aa
seonmin <seonmiki@student.42seoul.kr>
2024-05-18 21:08:12 +0900

    검색결과 있을 때 페이지 구현

--- src/components/Search/SearchResult/SearchResult.js
+++ src/components/Search/SearchResult/SearchResult.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NoneResult from "./noneResult/NoneResult";
+import ThumbNail from "../../ThumbNail/ThumbNail";
 
 const SearchResult = ({ games, keyworld }) => {
 
   	return <div className="searchResult">
-		{keyworld !== "검색 키워드" ? <NoneResult value={keyworld} key={keyworld}/> 
-		: <div className="searchResult">
+		{keyworld !== "검색 키워드" ? <NoneResult value={keyworld} /> 
+		: <div className="Result">
 			<div className="message">
 				<div className="text">
 				  <div className="head">
@@ -13,6 +14,14 @@ const SearchResult = ({ games, keyworld }) => {
 					<h1 className="h1">에 대한 검색결과에요!</h1>
 				  </div>
 				</div>
+			</div>
+			<div className="ResultThumbNail">
+				<ThumbNail type="small" />
+				{/* <div className="wrapper">
+					{games.map((_, i) => (
+						<ThumbNail type="small" key={i}/>
+					))}
+				</div> */}
 			</div>
 		</div>}
 	</div>
