11d10d4f07cdbe5d1a3b47eab4b299cc58ba0b49
seonmiki <127717117+seonmiki@users.noreply.github.com>
2024-05-19 23:41:02 +0900

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import { useContext, useRef, useState, useEffect } from "react";
-
+import { useRef, useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 import SearchBar from "../../components/Search/SearchBar/SearchBar";
 import OnSearch from "../../components/Search/OnSearch/OnSearch.js";
 import CategoryBox from "../../components/CategoryBox/CategoryBox";
 import Rank from "../../components/ThumbNail/Rank/Rank";
-import Banner from "../../components/Banner/Banner";
+import Banner from "../../components/Banner/Banner"
 import ThumbNail from "../../components/ThumbNail/ThumbNail";
 import { useSlidesPerView } from "../../common/util/useSliderPerView";
 import { getRankData } from "../../common/axios/rank.js";
@@ -16,13 +14,9 @@ import Loading from "../../components/Search/SearchResult/Loading/Loading.js";
 
 const Home = () => {
   const onSearch = useSelector((state) => state.onSearch);
-
-  const log = useContext(SearchContext);
-  const gameTabRef = useRef(null);
-
+  const gameTabRef = useRef({});
   const slidesPerView = useSlidesPerView(gameTabRef);
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
-
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -30,36 +24,38 @@ const Home = () => {
   // 랭킹 api 호출
   const [rankData, setRankData] = useState(null);
   useEffect(() => {
-    const fetchRankData = async () => {
+    const fetchData = async () => {
       setLoading(true);
       try {
-        const result = await getRankData();
-        setRankData(result);
+        const rankData = await getRankData();
+        setRankData(rankData);
         setLoading(false);
-      } catch (error) {
-        setError(error.message);
+      } catch (err) {
+        setError(err.message);
         setLoading(false);
       }
     };
-    fetchRankData();
+
+    fetchData();
   }, []);
 
+  // 추천 api 호출
   const [recommandData, setRecommandData] = useState(null);
   useEffect(() => {
-    const fetchRecommandData = async () => {
+    const fetchData = async () => {
       setLoading(true);
       try {
-        const result = await getRecommandData();
-        setRecommandData(result);
+        const recommandData = await getRecommandData();
+        setRecommandData(recommandData);
         setLoading(false);
-      } catch (error) {
-        setError(error.message);
+      } catch (err) {
+        setError(err.message);
         setLoading(false);
       }
     };
-    fetchRecommandData();
-  }, []);
 
+    fetchData();
+  }, []);
 
   if (loading)
 		return <Loading />
@@ -80,7 +76,18 @@ const Home = () => {
             <h1 className="name">'스위프'</h1>
             <h1>회원님이</h1>
           </div>
-
+          <h2>관심 있어 할만한 보드게임</h2>
+        </div>
+        <div className="slide" ref={gameTabRef} >
+          <Swiper slidesPerView={slidesPerView} spaceBetween={8}>
+            {recommandData && recommandData.map((r, i) => (
+              <SwiperSlide key={i}>
+                <ThumbNail id={r.id} img={r.imageUrl} name={r.name} info={r.description} tags={r.tags}type="small" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+    </article>
 
     {/* 오늘의 보드픽 랭킹 */}
     <div className="pickRank">
