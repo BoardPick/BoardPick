5b4a58cdbb47f5b5556a46a99e28649d3f02d7a6
seonmin <seonmiki@student.42seoul.kr>
2024-05-22 00:28:27 +0900

    Add: 최근 검색어 기록 없을 때

--- src/components/Search/OnSearch/OnSearch.js
+++ src/components/Search/OnSearch/OnSearch.js
@@ -13,13 +13,14 @@ const OnSearch = () => {
     return (
     <div className="recent">
         <h1 className="title">최근 검색어</h1>
-        <div className="tags">
+        <p>최근 검색어 내용이 없어요!</p>
+        {/* <div className="tags">
             {tags.map((tag, i) => {
                 return (<div className="tag" key={i}>
                     <Button key={i} text={tag} size={"s36"} type={"default"}></Button>
                 </div>)
             })}
-        </div>
+        </div> */}
     </div>
     )
 };
