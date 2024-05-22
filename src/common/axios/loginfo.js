1ca47b7492acfcc046294b1ad2219d824d7c6d79
yoojin <yoojin4719@gmail.com>
2024-05-23 05:10:20 +0900

    token test

--- src/common/axios/loginfo.js
+++ src/common/axios/loginfo.js
@@ -3,9 +3,18 @@ const apiRoot = axios.create({
   baseURL: "https://boardpick-server.store",
 });
 
-export const getLogInfo = async () => {
+export const getLogInfo = async (token) => {
   try {
-    const { data } = await apiRoot.get(`/api/user`);
+    const { data } = await apiRoot.post(
+      `/api/user`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      }
+    );
     return data;
   } catch (error) {
     throw error;
